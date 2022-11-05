4de65b12abfe98c6107a57d784c3c8edbd253960
Nikolay Nazarenko <nikola22.05.2001@mail.ru>
2022-11-05 20:15:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//№1
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+
+if (a > b){
+    Console.WriteLine($"max = {a} (первое число больше)");
+}
+else if (b>a){
+    Console.WriteLine($"max = {b} (второе число больше)");
+}
+else{
+    Console.WriteLine($"max = {b} (числа равны)");
+}
+
+//№2
+int[] nums = {0, 0, 0};
+int max = nums[0];
+Console.WriteLine("Введите три числа (по одному)");
+for (int i = 0; i<3; i++) {
+    nums[i] = int.Parse(Console.ReadLine());
+    if (i == 1){
+        max = nums[i];
+    }
+    else {
+        if (max < nums[i]){
+            max = nums[i];
+        }
+    }
+}
+Console.WriteLine($"Ответ: {max}");
+
+//№3
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0){
+    Console.WriteLine("Да (число четное)");
+}
+else{
+    Console.WriteLine("Нет (число нечетное)");
+}
+
+//№4
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int current = 2;
+for (int i = 1; i < n; i = i+2){
+    Console.Write(current + ", ");
+    current = current + 2;
+}
