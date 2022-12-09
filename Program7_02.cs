f8b6d7830a8c76a06dc186436f56b11f5f0d99ec
CrrazyKnight <115745398+CrrazyKnight@users.noreply.github.com>
2022-12-10 00:27:33 +0300

    Задание 7_02
    Крайне много неточностей в формулировке задачи. Генерацию массива взял преимущественно из предыдущего задания. Индексы берутся так, как мы считаем в жизни, а не как считает компьютер (массив из 3 элементов имеет индексы от 1 до 3). Вывожу сам массив (если таковой нужно сгенерировать) и число/ответ.

---
+++ Program7_02.cs
@@ -0,0 +1,34 @@
+void Search(){
+    Console.Write("Введите количество строк ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Введите количество столбцов  ");
+    int n = int.Parse(Console.ReadLine());
+    int[,] array = new int[m, n];
+
+
+    Console.Write("Введите позиции элемента для поиска через пробел: ");
+    string[] number = (Console.ReadLine()).Split(' ');
+    int number_x = int.Parse(number[0]);
+    int number_y = int.Parse(number[1]);
+
+    if ((number_x < m+1) & (number_y < n+1))
+    {
+        var rnd = new Random();
+        for (int i = 0; i < m; i++)
+        {
+            for (int a = 0; a < n; a++)
+            {
+                array[i, a] = rnd.Next(-10,10);
+                Console.Write(array[i, a] + "  ");
+            }
+            Console.WriteLine("\n");
+    }
+    Console.WriteLine(array[number_x-1, number_y-1]);
+    }
+   else
+   {
+    Console.WriteLine("Такого числа в массиве нет");
+   }
+}
+
+Search();
